4f0b9c3035c22c288cf8ce8982f722f3bbd6b1df
Michael Ben-Zvi <ingiman1@gmail.com>
2023-03-04 23:04:03 -0800

    comments

--- src/EZ-Template-extend/drive/odometry.cpp
+++ src/EZ-Template-extend/drive/odometry.cpp
@@ -6,7 +6,9 @@
 
 using namespace ez;
 
-void Drive::ez_odometry_task() { //COORDINATE SYSTEM: at orientation 0 robot moves towards positive x, clockwise turn is positive (CW+)
+void Drive::ez_odometry_task() { // COORDINATE SYSTEM: at orientation 0 robot
+                                 // moves towards positive x, clockwise turn is
+                                 // positive (CW+)
   int last_left_sensor = left_sensor();
   int last_right_sensor = right_sensor();
   int last_middle_sensor = middle_sensor();
@@ -17,9 +19,9 @@ void Drive::ez_odometry_task() { //COORDINATE SYSTEM: at orientation 0 robot mov
 
   std::uint32_t now = pros::millis();
 
-  while (true) {
+  while (true) { // enter odom loop
 
-    if(imu.is_calibrating()) {
+    if (imu.is_calibrating()) {
       continue;
     }
 
@@ -27,50 +29,65 @@ void Drive::ez_odometry_task() { //COORDINATE SYSTEM: at orientation 0 robot mov
 
     orientation.set_deg(get_gyro());
 
-    double left_distance = (double)(left_sensor() - last_left_sensor) / TICK_PER_INCH;
-    double right_distance = (double)(right_sensor() - last_right_sensor) / TICK_PER_INCH;
-    double middle_distance = (double)(middle_sensor() - last_middle_sensor) / MIDDLE_TICK_PER_INCH;
+    double left_distance =
+        (double)(left_sensor() - last_left_sensor) / TICK_PER_INCH;
+    double right_distance =
+        (double)(right_sensor() - last_right_sensor) / TICK_PER_INCH;
+    double middle_distance =
+        (double)(middle_sensor() - last_middle_sensor) /
+        MIDDLE_TICK_PER_INCH; // convert sensor values to inch values
 
     last_left_sensor = left_sensor();
     last_right_sensor = right_sensor();
     last_middle_sensor = middle_sensor();
 
-    double orientation_delta = Angle::shortest_error(last_orientation, orientation);
+    double orientation_delta =
+        Angle::shortest_error(last_orientation, orientation);
 
-    Angle orientation_average = Angle::from_rad(last_orientation.get_rad() + orientation_delta / 2);
+    Angle orientation_average =
+        Angle::from_rad(last_orientation.get_rad() + orientation_delta / 2);
 
-    //printf("Average Orientation: %f", (float)orientation_average.get_deg());
+    // printf("Average Orientation: %f", (float)orientation_average.get_deg());
 
     last_orientation.set_rad(orientation.get_rad());
 
     if (orientation_delta == 0) {
-      local_move = Vector2((left_distance + right_distance) / 2, middle_distance);
+      local_move =
+          Vector2((left_distance + right_distance) / 2, middle_distance);
       local_move = Vector2(right_distance, middle_distance);
     } else {
       // arc length = radius * theta
-      // find radius from both sides and add or subtract the offset from tracking center (by convention the arc is to the right of robot so L is far from arc center and R is close)
+      // find radius from both sides and add or subtract the offset from
+      // tracking center (by convention the arc is to the right of robot so L is
+      // far from arc center and R is close)
 
       double left_radius = left_distance / orientation_delta;
       double right_radius = right_distance / orientation_delta;
-      double lateral_radius = ((left_radius - l_width) + (right_radius + r_width)) / 2; // take average of both side's reading of radius 
-      lateral_radius = right_radius+r_width;
+      double lateral_radius =
+          ((left_radius - l_width) + (right_radius + r_width)) /
+          2; // take average of both side's reading of radius
+      lateral_radius = right_radius + r_width;
       double horizontal_radius = (middle_distance / orientation_delta) + length;
       local_move = // local move = chord length = 2 * radius * sin(theta/2)
-          Vector2(2 * lateral_radius * sin(orientation_delta / 2) , 2 * horizontal_radius * sin(orientation_delta / 2));
+          Vector2(2 * lateral_radius * sin(orientation_delta / 2),
+                  2 * horizontal_radius * sin(orientation_delta / 2));
     }
     if (local_move.get_magnitude() != 0) {
       Vector2 global_move = Vector2(local_move.x, local_move.y);
 
-      // local move is offset from global CS by average_orientation so we must rotate it
-      global_move.set_angle_direction( -local_move.get_angle_direction() + orientation_average); // local move.get_angle_direction is in coordinate system where CCW+ so we must invert it
-      
+      // local move is offset from global CS by average_orientation so we must
+      // rotate it
+      global_move.set_angle_direction(
+          -local_move.get_angle_direction() +
+          orientation_average); // local move.get_angle_direction is in
+                                // coordinate system where CCW+ so we must
+                                // invert it
 
       position.x += global_move.x;
-      position.y += global_move.y; 
+      position.y += global_move.y;
     }
 
     pros::Task::delay_until(&now, ez::util::DELAY_TIME);
-    
   }
 }
 
